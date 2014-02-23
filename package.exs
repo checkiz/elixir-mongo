1576b5cd64f2823f0037d3003166c67db62a1edb
Jerome <jerome.proost@checkiz.com>
2014-02-23 23:44:04 +0100

    add Expm package

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "elixir-mongo", description: "MongoDB driver for Elixir",
+                 version: "0.2", keywords: ["Elixir", "MongoDB", "bson", "database", "driver"], 
+                 maintainers: [[name: "Jerome Proost", email: "elixir-mongo@askabird.com"]],
+                 repositories: [[github: "checkiz/elixir-mongo", tag: "0.2"]])
