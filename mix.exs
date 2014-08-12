ffec225cc62728228edbe8e8cec1a141220b299d
Jerome <jerome.proost@checkiz.com>
2014-08-12 14:52:50 +0200

    Expm is dead, long live Hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Mongo.Mixfile do
       version: "0.3.1",
       elixir: "~> 0.15.1",
       source_url: "https://github.com/checkiz/elixir-mongo",
+      description: "MongoDB driver for Elixir",
       deps: deps(Mix.env),
+      package: package,
       docs: &docs/0 ]
   end
 
@@ -22,14 +24,14 @@ defmodule Mongo.Mixfile do
   # Returns the list of dependencies for prod
   defp deps(:prod) do
     [
-      {:bson, github: "checkiz/elixir-bson"}
+      bson: "~> 0.3.1"
     ]
   end
 
   # Returns the list of dependencies for docs
   defp deps(:docs) do
     deps(:prod) ++
-      [{ :ex_doc, github: "elixir-lang/ex_doc" }]
+      [ex_doc: "~> 0.5.1"]
   end
   defp deps(_), do: deps(:prod)
 
@@ -37,6 +39,15 @@ defmodule Mongo.Mixfile do
     [ #readme: false,
       #main: "README",
       source_ref: System.cmd("git rev-parse --verify --quiet HEAD") ]
+  end
+
+  defp package do
+    [ contributors: ["jerp"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/checkiz/elixir-mongo",
+        "Documentation" => "https://checkiz.github.io/elixir-mongo"
+      } ]
   end
 
 end
